d714b40b9e4ea5ee63ff86fda669a02a4bbd837f
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-05-23 12:06:24 -0500

    Cleanup leftovers from project copy

--- TinkersSatchel.cs
+++ TinkersSatchel.cs
@@ -8,19 +8,11 @@ using Path = System.IO.Path;
 using TILER2;
 using static TILER2.MiscUtil;
 
-//TODO:
-// Add missing documentation in... a whole lotta places... whoops.
-// Change H3AD-5T V2 to a green item if removing the stomp effect?
-// Add lots of missing items!
-// Watch for R2API.StatsAPI or similar, for use in some items like Bitter Root, Mysterious Vial, Rusty Jetpack
-// Find out how to safely and instantaneously change money counter, for cases like Life Savings that shouldn't have the sound effects
-// Engineer turrets spammed errors during FixedUpdate and/or RecalculateStats at one point?? Probably resolved now but keep an eye out for things like this
-
 namespace ThinkInvisible.TinkersSatchel {
     [BepInPlugin(ModGuid, ModName, ModVer)]
     [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
     [BepInDependency(TILER2Plugin.ModGuid, "1.2.1")]
-    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI), nameof(ResourcesAPI), nameof(PlayerAPI), nameof(PrefabAPI), nameof(BuffAPI), nameof(LoadoutAPI))]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI), nameof(ResourcesAPI), nameof(PlayerAPI), nameof(PrefabAPI))]
     public class TinkersSatchelPlugin:BaseUnityPlugin {
         public const string ModVer = "1.0.0";
         public const string ModName = "TinkersSatchel";
