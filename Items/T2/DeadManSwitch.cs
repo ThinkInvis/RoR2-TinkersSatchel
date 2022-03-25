b04d1357c1ec2e2cd7edd63395382bd50b434e76
ThinkInvisible <thinkinvis@gmail.com>
2022-03-25 14:01:28 -0500

    Fix config on ItemIsAIBlacklisted

--- Items/T2/DeadManSwitch.cs
+++ Items/T2/DeadManSwitch.cs
@@ -14,7 +14,7 @@ namespace ThinkInvisible.TinkersSatchel {
         public override string displayName => "Pulse Monitor";
         public override ItemTier itemTier => ItemTier.Tier2;
         public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.EquipmentRelated, ItemTag.Utility, ItemTag.LowHealth });
-        public override bool itemIsAIBlacklisted => true;
+        public override bool itemIsAIBlacklisted { get; protected set; } = true;
 
         protected override string GetNameString(string langid = null) => displayName;
         protected override string GetPickupString(string langid = null) => "Activate your equipment for free at low health.";
