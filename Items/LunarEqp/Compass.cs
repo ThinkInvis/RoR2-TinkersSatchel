0db7a1beab242a91176a1d8300dd70a0efbb9b0f
ThinkInvisible <thinkinvis@gmail.com>
2022-04-15 18:37:59 -0500

    Relocate item-specific components

--- Items/LunarEqp/Compass.cs
+++ Items/LunarEqp/Compass.cs
@@ -80,4 +80,26 @@ namespace ThinkInvisible.TinkersSatchel {
 	}
 
     public class SilverCompassFlag : MonoBehaviour {}
+
+    public class TargetSpinnerAnim : MonoBehaviour {
+        public float rotateTime = 0.5f;
+        public float delayTime = 1f;
+        public Vector3 rotateAxis;
+
+        private float targPos = -1f;
+        private float currVel;
+        private float currPos;
+        private float stopwatch;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by Unity Engine.")]
+        void Update() {
+            stopwatch -= Time.deltaTime;
+            if(stopwatch < 0f) {
+                targPos = Random.value * Mathf.PI * 2;
+                stopwatch = rotateTime + delayTime;
+            }
+            currPos = Mathf.SmoothDampAngle(currPos, targPos, ref currVel, rotateTime);
+            this.gameObject.transform.Rotate(rotateAxis, currVel);
+        }
+    }
 }
