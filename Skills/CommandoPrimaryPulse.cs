ba3c1abe1b23aae164705c22d0e92880e36df238
ThinkInvisible <thinkinvis@gmail.com>
2022-04-16 11:26:41 -0500

    Add code for new skills

---
+++ Skills/CommandoPrimaryPulse.cs
@@ -0,0 +1,158 @@
+﻿using RoR2;
+using UnityEngine;
+using System.Collections.ObjectModel;
+using TILER2;
+using static TILER2.MiscUtil;
+using static R2API.RecalculateStatsAPI;
+using R2API;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.AddressableAssets;
+using EntityStates;
+using RoR2.Projectile;
+using RoR2.Skills;
+
+namespace ThinkInvisible.TinkersSatchel {
+	public class CommandoPrimaryPulse : T2Module<CommandoPrimaryPulse> {
+
+		////// Module Data //////
+
+		public override AutoConfigFlags enabledConfigFlags => AutoConfigFlags.DeferForever | AutoConfigFlags.PreventNetMismatch;
+
+
+
+		////// Other Fields/Properties //////
+
+		public SkillDef skillDef { get; private set; }
+		bool setupSucceeded = false;
+		SkillFamily targetSkillFamily;
+
+
+
+		////// TILER2 Module Setup //////
+
+		public CommandoPrimaryPulse() {
+		}
+
+		public override void RefreshPermanentLanguage() {
+			permanentGenericLanguageTokens.Add("TKSAT_COMMANDO_PRIMARY_PULSE_NAME", "Pulse");
+			permanentGenericLanguageTokens.Add("TKSAT_COMMANDO_PRIMARY_PULSE_DESCRIPTION", "Rapidly shoot an enemy 4 times with high recoil. <style=cIsDamage>Damage</style> per shot ramps from <style=cIsDamage>75% to 150%</style> over the course of the burst.");
+			base.RefreshPermanentLanguage();
+		}
+
+		public override void SetupAttributes() {
+			base.SetupAttributes();
+
+			//load custom assets
+			skillDef = TinkersSatchelPlugin.resources.LoadAsset<SkillDef>("Assets/TinkersSatchel/SkillDefs/CommandoPrimaryPulse.asset");
+
+			//load vanilla assets
+			targetSkillFamily = Addressables.LoadAssetAsync<SkillFamily>("RoR2/Base/Commando/CommandoBodyPrimaryFamily.asset")
+				.WaitForCompletion();
+
+			//R2API catalog reg
+			skillDef.activationState = ContentAddition.AddEntityState<Fire>(out bool entStateDidSucceed);
+
+			if(!entStateDidSucceed) {
+				TinkersSatchelPlugin._logger.LogError("EntityState setup failed on CommandoPrimaryPulse! Skill will not appear nor function.");
+			} else if(!ContentAddition.AddSkillDef(skillDef)) {
+				TinkersSatchelPlugin._logger.LogError("SkillDef setup failed on CommandoPrimaryPulse! Skill will not appear nor function.");
+			} else {
+				setupSucceeded = true;
+			}
+		}
+
+		public override void Install() {
+			base.Install();
+			if(setupSucceeded) {
+				targetSkillFamily.AddVariant(skillDef);
+			}
+		}
+
+		public override void Uninstall() {
+			base.Uninstall();
+			if(setupSucceeded) {
+				targetSkillFamily.RemoveVariant(skillDef);
+			}
+		}
+
+
+
+		////// Hooks //////
+
+
+		////// Skill States //////
+
+		public class Fire : BaseSkillState {
+			private void FireBullet(string targetMuzzle, int shotIndex) {
+				var aim = GetAimRay();
+				Util.PlaySound(EntityStates.Commando.CommandoWeapon.FirePistol2.firePistolSoundString, gameObject);
+				if(EntityStates.Commando.CommandoWeapon.FirePistol2.muzzleEffectPrefab)
+					EffectManager.SimpleMuzzleFlash(EntityStates.Commando.CommandoWeapon.FirePistol2.muzzleEffectPrefab, gameObject, targetMuzzle, false);
+				AddRecoil(-0.7f * recoilAmplitude, -1f * recoilAmplitude, -0.1f * recoilAmplitude, 0.1f * recoilAmplitude);
+				if(isAuthority) {
+					new BulletAttack {
+						owner = gameObject,
+						weapon = gameObject,
+						origin = aim.origin,
+						aimVector = aim.direction,
+						minSpread = 0f,
+						maxSpread = characterBody.spreadBloomAngle,
+						damage = Mathf.Lerp(initialDamageCoefficient, finalDamageCoefficient, (float)shotIndex/((float)burstCount -1f)) * damageStat,
+						force = force,
+						tracerEffectPrefab = EntityStates.Commando.CommandoWeapon.FirePistol2.tracerEffectPrefab,
+						muzzleName = targetMuzzle,
+						hitEffectPrefab = EntityStates.Commando.CommandoWeapon.FirePistol2.hitEffectPrefab,
+						isCrit = Util.CheckRoll(critStat, characterBody.master),
+						radius = 0.1f,
+						smartCollision = true
+					}.Fire();
+				}
+				characterBody.AddSpreadBloom(spreadBloomValue);
+			}
+
+			public override void OnEnter() {
+				base.OnEnter();
+				duration = (baseDurationPerShot * burstCount + baseEndLagDuration) / attackSpeedStat;
+				StartAimMode(GetAimRay(), 3f, false);
+				stopwatch = 0;
+				shotsFired = 0;
+			}
+
+			public override void FixedUpdate() {
+				base.FixedUpdate();
+				stopwatch -= Time.fixedDeltaTime;
+				while(stopwatch <= 0f && shotsFired < burstCount) {
+					if(shotsFired % 2 == 0) {
+						PlayAnimation("Gesture Additive, Left", "FirePistol, Left");
+						FireBullet("MuzzleLeft", shotsFired);
+					} else {
+						PlayAnimation("Gesture Additive, Right", "FirePistol, Right");
+						FireBullet("MuzzleRight", shotsFired);
+					}
+					shotsFired++;
+					stopwatch += baseDurationPerShot / attackSpeedStat;
+				}
+				if(isAuthority && fixedAge > duration && shotsFired >= burstCount) {
+					outer.SetNextStateToMain();
+				}
+			}
+
+			public override InterruptPriority GetMinimumInterruptPriority() {
+				return InterruptPriority.Skill;
+			}
+
+			public static float initialDamageCoefficient = 0.75f;
+			public static float finalDamageCoefficient = 1.5f;
+			public static float force = 0f;
+			public static float baseEndLagDuration = 0.3f;
+			public static float baseDurationPerShot = 0.1f;
+			public static float recoilAmplitude = 2f;
+			public static float spreadBloomValue = 0.1f;
+			public static int burstCount = 4;
+			private float duration;
+			private float stopwatch;
+			private int shotsFired;
+		}
+	}
+}
---
+++ Skills/CommandoUtilityJinkJet.cs
@@ -0,0 +1,130 @@
+﻿using RoR2;
+using UnityEngine;
+using System.Collections.ObjectModel;
+using TILER2;
+using static TILER2.MiscUtil;
+using static R2API.RecalculateStatsAPI;
+using R2API;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.AddressableAssets;
+using EntityStates;
+using RoR2.Projectile;
+using RoR2.Skills;
+using UnityEngine.Networking;
+
+namespace ThinkInvisible.TinkersSatchel {
+	public class CommandoUtilityJinkJet : T2Module<CommandoUtilityJinkJet> {
+
+		////// Module Data //////
+
+		public override AutoConfigFlags enabledConfigFlags => AutoConfigFlags.DeferForever | AutoConfigFlags.PreventNetMismatch;
+
+
+
+		////// Other Fields/Properties //////
+
+		public SkillDef skillDef { get; private set; }
+		bool setupSucceeded = false;
+		SkillFamily targetSkillFamily;
+
+
+
+		////// TILER2 Module Setup //////
+
+		public CommandoUtilityJinkJet() {
+		}
+
+		public override void RefreshPermanentLanguage() {
+			permanentGenericLanguageTokens.Add("TKSAT_COMMANDO_UTILITY_JINKJET_NAME", "Jink Jet");
+			permanentGenericLanguageTokens.Add("TKSAT_COMMANDO_UTILITY_JINKJET_DESCRIPTION", "Perform a very short dodge that <style=cIsUtility>instantly boosts you to sprint speed</style>. Hold up to 3 charges.");
+			base.RefreshPermanentLanguage();
+		}
+
+		public override void SetupAttributes() {
+			base.SetupAttributes();
+
+			//load custom assets
+			skillDef = TinkersSatchelPlugin.resources.LoadAsset<SkillDef>("Assets/TinkersSatchel/SkillDefs/CommandoUtilityJinkJet.asset");
+
+			//load vanilla assets
+			targetSkillFamily = Addressables.LoadAssetAsync<SkillFamily>("RoR2/Base/Commando/CommandoBodyUtilityFamily.asset")
+				.WaitForCompletion();
+
+			//R2API catalog reg
+			skillDef.activationState = ContentAddition.AddEntityState<QuickDodge>(out bool entStateDidSucceed);
+
+			if(!entStateDidSucceed) {
+				TinkersSatchelPlugin._logger.LogError("EntityState setup failed on CommandoUtilityJinkJet! Skill will not appear nor function.");
+			} else if(!ContentAddition.AddSkillDef(skillDef)) {
+				TinkersSatchelPlugin._logger.LogError("SkillDef setup failed on CommandoUtilityJinkJet! Skill will not appear nor function.");
+			} else {
+				setupSucceeded = true;
+			}
+		}
+
+		public override void Install() {
+			base.Install();
+			if(setupSucceeded) {
+				targetSkillFamily.AddVariant(skillDef);
+			}
+		}
+
+		public override void Uninstall() {
+			base.Uninstall();
+			if(setupSucceeded) {
+				targetSkillFamily.RemoveVariant(skillDef);
+			}
+		}
+
+
+
+		////// Hooks //////
+
+
+		////// Skill States //////
+
+		public class QuickDodge : BaseState {
+			public override void OnEnter() {
+				base.OnEnter();
+				Util.PlaySound("Play_commando_M2_grenade_explo", gameObject);
+				var animator = GetModelAnimator();
+				ChildLocator component = animator.GetComponent<ChildLocator>();
+				var skillForward = Vector3.zero;
+				if(isAuthority && inputBank && characterDirection)
+					skillForward = (
+							((inputBank.moveVector == Vector3.zero) ? characterDirection.forward : inputBank.moveVector)
+							+ (inputBank.jump.down ? (Vector3.up / 2f) : Vector3.zero)
+						).normalized;
+				var animForward = characterDirection ? characterDirection.forward : skillForward;
+				var forwardAdjust = Vector3.Dot(skillForward, animForward);
+				var rightAdjust = Vector3.Dot(skillForward, Vector3.Cross(Vector3.up, animForward));
+				animator.SetFloat("forwardSpeed", forwardAdjust, 0.1f, Time.fixedDeltaTime);
+				animator.SetFloat("rightSpeed", rightAdjust, 0.1f, Time.fixedDeltaTime);
+				if(Mathf.Abs(forwardAdjust) > Mathf.Abs(rightAdjust))
+					PlayAnimation("Body", (forwardAdjust > 0f) ? "DodgeForward" : "DodgeBackward", "Dodge.playbackRate", duration);
+				else
+					PlayAnimation("Body", (rightAdjust > 0f) ? "DodgeRight" : "DodgeLeft", "Dodge.playbackRate", duration);
+				var je = EntityStates.Commando.DodgeState.jetEffect;
+				if(je) {
+					var tsfL = component.FindChild("LeftJet");
+					var tsfR = component.FindChild("RightJet");
+					if(tsfL) UnityEngine.Object.Instantiate<GameObject>(je, tsfL);
+					if(tsfR) UnityEngine.Object.Instantiate<GameObject>(je, tsfR);
+				}
+				float sprintMult = 1;
+				if(characterBody) {
+					characterBody.isSprinting = true;
+					sprintMult = characterBody.sprintingSpeedMultiplier;
+                }
+				if(characterMotor)
+					characterMotor.velocity = skillForward * moveSpeedStat * sprintMult;
+				if(isAuthority)
+					outer.SetNextStateToMain();
+			}
+
+			[SerializeField]
+			public float duration = 0.15f;
+		}
+	}
+}
