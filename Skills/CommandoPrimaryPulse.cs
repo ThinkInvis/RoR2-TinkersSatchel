276016d41cc64bf88c47ce2f49352cbeb72cd339
ThinkInvisible <thinkinvis@gmail.com>
2022-04-16 12:12:51 -0500

    Tweak skill stats

--- Skills/CommandoPrimaryPulse.cs
+++ Skills/CommandoPrimaryPulse.cs
@@ -139,9 +139,9 @@ namespace ThinkInvisible.TinkersSatchel {
 			public static float initialDamageCoefficient = 0.75f;
 			public static float finalDamageCoefficient = 1.5f;
 			public static float force = 0f;
-			public static float baseEndLagDuration = 0.3f;
+			public static float baseEndLagDuration = 0.4f;
 			public static float baseDurationPerShot = 0.08f;
-			public static float recoilAmplitude = 5f;
+			public static float recoilAmplitude = 5.5f;
 			public static float spreadBloomValue = 0.25f;
 			public static int burstCount = 4;
 			private float duration;
