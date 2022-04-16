e5247cffb81164d7b1373e979c587a49e8b7a1e6
ThinkInvisible <thinkinvis@gmail.com>
2022-04-16 16:32:17 -0500

    Reduce up vector force

--- Skills/CommandoUtilityJinkJet.cs
+++ Skills/CommandoUtilityJinkJet.cs
@@ -86,7 +86,7 @@ namespace ThinkInvisible.TinkersSatchel {
 				var cLoc = animator.GetComponent<ChildLocator>();
 				var skillForward = GetAimRay().direction;
 				if(characterMotor && characterMotor.isGrounded)
-					skillForward.y += 1f;
+					skillForward.y += 0.5f;
 				skillForward.Normalize();
 				var je = EntityStates.Commando.DodgeState.jetEffect;
 				if(je) {
