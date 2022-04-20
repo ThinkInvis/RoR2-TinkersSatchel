d3c5abdaf49a96057fb1097a86655763a9bb8f2b
ThinkInvisible <thinkinvis@gmail.com>
2022-04-20 17:15:40 -0500

    Add CI compat (enabled check only so far)

---
+++ Compat/ClassicItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ThinkInvisible.TinkersSatchel {
+    ///<summary>
+    ///Provides safe hooks for the ClassicItems mod. Check Compat_ClassicItems.enabled before using any other contained members.
+    ///</summary>
+    public static class Compat_ClassicItems {
+
+        private static bool? _enabled;
+        public static bool enabled {
+            get {
+                if(_enabled == null) _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.ThinkInvisible.ClassicItems");
+                return (bool)_enabled;
+            }
+        }
+    }
+}
