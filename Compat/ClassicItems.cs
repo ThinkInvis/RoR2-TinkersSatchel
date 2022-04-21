81ec6f416623fced5448b4d46f75a79168771eef
ThinkInvisible <thinkinvis@gmail.com>
2022-04-21 09:16:56 -0500

    Add CI compat functions

--- Compat/ClassicItems.cs
+++ Compat/ClassicItems.cs
@@ -1,10 +1,32 @@
-﻿using System.Runtime.CompilerServices;
+﻿using RoR2;
+using RoR2.Skills;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace ThinkInvisible.TinkersSatchel {
     ///<summary>
     ///Provides safe hooks for the ClassicItems mod. Check Compat_ClassicItems.enabled before using any other contained members.
     ///</summary>
     public static class Compat_ClassicItems {
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public static int CheckProc(CharacterBody body, EquipmentDef eqp) {
+            return ClassicItems.Embryo.CheckLastEmbryoProc(body, eqp);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public static int CheckProc(EquipmentSlot slot, EquipmentDef eqp) {
+            return ClassicItems.Embryo.CheckLastEmbryoProc(slot, eqp);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public static void RegisterHook(EquipmentDef equipment, string descAppendToken = null, Func<string> configDisplayNameDelegate = null, Action installHooksDelegate = null, Action uninstallHooksDelegate = null, Action<CharacterBody> addComponentsDelegate = null, Action setupAttributesDelegate = null, Action setupConfigDelegate = null) {
+            ClassicItems.Embryo.RegisterHook(equipment, descAppendToken, configDisplayNameDelegate, installHooksDelegate, uninstallHooksDelegate, addComponentsDelegate, setupAttributesDelegate, setupConfigDelegate);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public static bool RegisterScepterSkill(SkillDef replacingDef, string targetBodyName, SkillSlot targetSlot, int targetVariant) {
+            return ClassicItems.Scepter.instance.RegisterScepterSkill(replacingDef, targetBodyName, targetSlot, targetVariant);
+        }
 
         private static bool? _enabled;
         public static bool enabled {
