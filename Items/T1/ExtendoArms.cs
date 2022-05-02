6e4f5d05e052d28fd74ad4000f496c3e208abbd8
ThinkInvisible <thinkinvis@gmail.com>
2022-05-02 09:40:58 -0500

    Add functionality on PBAoEs

--- Items/T1/ExtendoArms.cs
+++ Items/T1/ExtendoArms.cs
@@ -20,8 +20,8 @@ namespace ThinkInvisible.TinkersSatchel {
         public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] {ItemTag.Damage});
 
         protected override string GetNameString(string langid = null) => displayName;
-        protected override string GetPickupString(string langid = null) => "Melee attacks reach farther and deal slightly more damage.";
-        protected override string GetDescString(string langid = null) => $"All your melee attacks gain <style=cIsUtility>{Pct(resizeAmount)} range <style=cStack>(+{Pct(resizeAmount)} per stack)</style></style> and <style=cIsDamage>{Pct(damageAmount)} damage <style=cStack>(+{Pct(damageAmount)} per stack)</style></style>.";
+        protected override string GetPickupString(string langid = null) => "Melee and PBAoE attacks reach farther and deal slightly more damage.";
+        protected override string GetDescString(string langid = null) => $"All your melee attacks, as well as AoEs within {pbaoeRange:N1} meters of your location, gain <style=cIsUtility>{Pct(resizeAmount)} range <style=cStack>(+{Pct(resizeAmount)} per stack)</style></style> and <style=cIsDamage>{Pct(damageAmount)} damage <style=cStack>(+{Pct(damageAmount)} per stack)</style></style>.";
         protected override string GetLoreString(string langid = null) => "";
 
 
@@ -38,10 +38,15 @@ namespace ThinkInvisible.TinkersSatchel {
         [AutoConfig("Melee attack damage increase per stack, linear.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
         public float damageAmount { get; private set; } = 0.0625f;
 
+        [AutoConfigRoOSlider("{0:N1}", 0f, 10f)]
+        [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
+        [AutoConfig("Maximum distance between character and center of an AoE to count as a PBAoE.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
+        public float pbaoeRange { get; private set; } = 5f;
+
 
 
         ////// TILER2 Module Setup //////
-        
+
         public ExtendoArms() {
             modelResource = TinkersSatchelPlugin.resources.LoadAsset<GameObject>("Assets/TinkersSatchel/Prefabs/Items/ExtendoArms.prefab");
             iconResource = TinkersSatchelPlugin.resources.LoadAsset<Sprite>("Assets/TinkersSatchel/Textures/ItemIcons/extendoArmsIcon.png");
@@ -54,17 +59,34 @@ namespace ThinkInvisible.TinkersSatchel {
         public override void Install() {
             base.Install();
             IL.RoR2.OverlapAttack.Fire += OverlapAttack_Fire;
+            On.RoR2.BlastAttack.Fire += BlastAttack_Fire;
         }
-
 
         public override void Uninstall() {
             base.Uninstall();
             IL.RoR2.OverlapAttack.Fire -= OverlapAttack_Fire;
+            On.RoR2.BlastAttack.Fire -= BlastAttack_Fire;
         }
 
 
 
         ////// Hooks //////
+
+        private BlastAttack.Result BlastAttack_Fire(On.RoR2.BlastAttack.orig_Fire orig, BlastAttack self) {
+            var origDamage = self.baseDamage;
+            var origRadius = self.radius;
+            if(self.attacker && self.attacker.TryGetComponent<CharacterBody>(out var attackerBody)) {
+                if(Vector3.Distance(attackerBody.corePosition, self.position) < pbaoeRange) {
+                    var count = GetCount(attackerBody);
+                    self.baseDamage *= 1f + count * damageAmount;
+                    self.radius *= 1f + count * resizeAmount;
+                }
+            }
+            var retv = orig(self);
+            self.baseDamage = origDamage;
+            self.radius = origRadius;
+            return retv;
+        }
 
         private void OverlapAttack_Fire(ILContext il) {
             var c = new ILCursor(il);
