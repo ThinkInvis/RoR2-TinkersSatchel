303eeded79cd6e69f7d6465722ec629f56d27454
ThinkInvisible <thinkinvis@gmail.com>
2022-07-23 15:46:28 -0500

    Reduce movement grace period

--- Items/Boss/ExtraEquipment.cs
+++ Items/Boss/ExtraEquipment.cs
@@ -29,7 +29,7 @@ namespace ThinkInvisible.TinkersSatchel {
 
 		[AutoConfigRoOSlider("{0:N1} s", 0f, 5f)]
 		[AutoConfig("Time required to register a movement stop, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
-		public float moveGracePeriod { get; private set; } = 3f;
+		public float moveGracePeriod { get; private set; } = 2f;
 
 		[AutoConfigRoOSlider("{0:N1} s", 0f, 5f)]
 		[AutoConfig("Cycle time of extra slots, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
