f71c914593e16f2e38825bfeef268072805e7e31
ThinkInvisible <thinkinvis@gmail.com>
2023-04-26 15:03:37 -0500

    Increase velocity, fix direction

--- Items/Eqp/Dodge.cs
+++ Items/Eqp/Dodge.cs
@@ -23,7 +23,7 @@ namespace ThinkInvisible.TinkersSatchel {
         [AutoConfigRoOSlider("{0:N1} m/s", 0f, 50f)]
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
         [AutoConfig("Instant speed to add on equipment activation.", AutoConfigFlags.None, 0f, float.MaxValue)]
-        public float evadeBurstSpeed { get; private set; } = 15f;
+        public float evadeBurstSpeed { get; private set; } = 60f;
 
         [AutoConfigRoOSlider("{0:N1} s", 0f, 10f)]
         [AutoConfig("Duration of the invulnerability effect.", AutoConfigFlags.PreventNetMismatch | AutoConfigFlags.DeferForever, 0f, float.MaxValue)]
@@ -78,8 +78,9 @@ namespace ThinkInvisible.TinkersSatchel {
             if(!slot.characterBody) return false;
 
             if(slot.characterBody.characterMotor && slot.characterBody.characterDirection) {
-                var boostVec = slot.characterBody.characterDirection.moveVector;
-                if(boostVec == Vector3.zero) boostVec = slot.characterBody.characterDirection.forward;
+                var boostVec = slot.characterBody.characterDirection.forward;
+                if(slot.inputBank && slot.inputBank.moveVector != Vector3.zero)
+                    boostVec = slot.inputBank.moveVector;
                 boostVec.y = 0f;
                 boostVec = boostVec.normalized;
                 slot.characterBody.characterMotor.velocity += boostVec * evadeBurstSpeed;
