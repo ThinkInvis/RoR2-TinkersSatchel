0cb4cd914ba9317f4cf5796bdcb4d37e3bd28c32
ThinkInvisible <thinkinvis@gmail.com>
2024-06-30 10:16:08 -0500

    Allow ProjectileSteerTowardsTargetRB prediction on clients

--- Skills/CommandoSpecialPlasmaGrenade.cs
+++ Skills/CommandoSpecialPlasmaGrenade.cs
@@ -161,10 +161,6 @@ namespace ThinkInvisible.TinkersSatchel {
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by Unity Engine.")]
 		private void Start() {
-			if(!NetworkServer.active) {
-				enabled = false;
-				return;
-			}
 			targetComponent = GetComponent<ProjectileTargetComponent>();
 			rb = GetComponent<Rigidbody>();
 		}
