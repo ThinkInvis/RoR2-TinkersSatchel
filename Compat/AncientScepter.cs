c57f0e3f63cd90677a685d4c06bf7b906617fe7d
ThinkInvisible <thinkinvis@gmail.com>
2024-07-04 13:06:02 -0500

    Make compat classes Internal

--- Compat/AncientScepter.cs
+++ Compat/AncientScepter.cs
@@ -6,14 +6,14 @@ namespace ThinkInvisible.TinkersSatchel {
     ///<summary>
     ///Provides safe hooks for the Standalone Ancient Scepter mod. Check Compat_AncientScepter.enabled before using any other contained members.
     ///</summary>
-    public static class Compat_AncientScepter {
+    internal static class Compat_AncientScepter {
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static bool RegisterScepterSkill(SkillDef replacingDef, string targetBodyName, SkillDef targetVariantDef) {
+        internal static bool RegisterScepterSkill(SkillDef replacingDef, string targetBodyName, SkillDef targetVariantDef) {
             return AncientScepter.AncientScepterItem.instance.RegisterScepterSkill(replacingDef, targetBodyName, targetVariantDef);
         }
 
         private static bool? _enabled;
-        public static bool enabled {
+        internal static bool enabled {
             get {
                 if(_enabled == null) _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.DestroyedClone.AncientScepter");
                 return (bool)_enabled;
--- Compat/Dronemeld.cs
+++ Compat/Dronemeld.cs
@@ -5,25 +5,25 @@ namespace ThinkInvisible.TinkersSatchel {
     ///<summary>
     ///Provides safe hooks for the Dronemeld mod. Check Compat_Dronemeld.enabled before using any other contained members except SafeGetCount.
     ///</summary>
-    public static class Compat_Dronemeld {
+    internal static class Compat_Dronemeld {
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static int SafeGetStackCount(Inventory inv) {
+        internal static int SafeGetStackCount(Inventory inv) {
             if(!enabled) return 0;
             return GetStackCount(inv);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static int GetStackCount(Inventory inv) {
+        internal static int GetStackCount(Inventory inv) {
             return inv.GetItemCount(Dronemeld.DronemeldPlugin.stackItem);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        public static CharacterMaster TryApply(CharacterMaster ownerMaster, string targetPrefabName) {
+        internal static CharacterMaster TryApply(CharacterMaster ownerMaster, string targetPrefabName) {
             return Dronemeld.DronemeldPlugin.TryApply(ownerMaster, targetPrefabName);
         }
 
         private static bool? _enabled;
-        public static bool enabled {
+        internal static bool enabled {
             get {
                 if(_enabled == null) {
                     if(BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue("com.ThinkInvisible.Dronemeld", out var pi)) {
