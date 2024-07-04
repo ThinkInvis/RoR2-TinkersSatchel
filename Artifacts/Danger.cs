050bc89ce548b6dadeddee22cf578fde8a9b3463
ThinkInvisible <thinkinvis@gmail.com>
2024-07-04 12:12:09 -0500

    Migrate curse OSP from Artifact of Danger to a tweak

--- Artifacts/Danger.cs
+++ Artifacts/Danger.cs
@@ -8,14 +8,6 @@ using UnityEngine;
 namespace ThinkInvisible.TinkersSatchel {
     public class Danger : Artifact<Danger> {
 
-        ////// Config //////
-
-        [AutoConfig("If true, disabling this artifact will prevent curses (max HP reduction) from removing OHP.",
-            AutoConfigFlags.PreventNetMismatch)]
-        public bool preventCurseWhileOff { get; private set; } = false;
-
-
-
         ////// TILER2 Module Setup //////
         
         public Danger() {
@@ -48,17 +40,6 @@ namespace ThinkInvisible.TinkersSatchel {
                 });
             } else {
                 TinkersSatchelPlugin._logger.LogError("failed to apply IL patch (Artifact of Danger, set OHP flag)! Artifact will not prevent OHP while enabled.");
-            }
-
-            ILFound = c.TryGotoNext(
-                x=>x.MatchCallOrCallvirt<Mathf>("Max"),
-                x=>x.MatchCallOrCallvirt<CharacterBody>("set_oneShotProtectionFraction"));
-            if(ILFound) {
-                c.Index++;
-                c.Emit(OpCodes.Ldarg_0);
-                c.EmitDelegate<Func<float,CharacterBody,float>>((origFrac,body)=>{return preventCurseWhileOff ? body.oneShotProtectionFraction : origFrac;});
-            } else {
-                TinkersSatchelPlugin._logger.LogError("failed to apply IL patch (Artifact of Danger, set OHP fraction)! Artifact's PreventCurseWhileOff config will not work.");
             }
         }
     }
---
+++ Tweaks/CursesKeepOSP.cs
@@ -0,0 +1,44 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using TILER2;
+using UnityEngine;
+
+namespace ThinkInvisible.TinkersSatchel {
+    public class CursesKeepOSP : T2Module<CursesKeepOSP> {
+
+        ////// TILER2 Module Setup //////
+        
+        public override bool managedEnable => true;
+
+        public override void Install() {
+            base.Install();
+            IL.RoR2.CharacterBody.RecalculateStats += IL_CBRecalcStats;
+        }
+
+        public override void Uninstall() {
+            base.Uninstall();
+            IL.RoR2.CharacterBody.RecalculateStats -= IL_CBRecalcStats;
+        }
+
+
+
+        ////// Hooks //////
+
+        private void IL_CBRecalcStats(ILContext il) {
+            ILCursor c = new(il);
+
+            bool ILFound = c.TryGotoNext(
+                x => x.MatchCallOrCallvirt<Mathf>("Max"),
+                x => x.MatchCallOrCallvirt<CharacterBody>("set_oneShotProtectionFraction"));
+            if(ILFound) {
+                c.Index++;
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<float, CharacterBody, float>>((origFrac, body) => { return body.oneShotProtectionFraction; });
+            } else {
+                TinkersSatchelPlugin._logger.LogError("failed to apply IL patch (CurseKeepOSP)! CurseKeepOSP module will not work.");
+            }
+        }
+    }
+}
