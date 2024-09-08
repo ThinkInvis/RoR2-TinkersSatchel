6444b87a50cbfd0c578037241fd0ac070e680a8f
ThinkInvisible <thinkinvis@gmail.com>
2024-09-08 14:49:54 -0500

    Rework Negative Feedback Loop

--- Items/T2/DamageBuffer.cs
+++ Items/T2/DamageBuffer.cs
@@ -27,26 +27,18 @@ namespace ThinkInvisible.TinkersSatchel {
 
         [AutoConfigRoOSlider("{0:P0}", 0f, 1f)]
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
-        [AutoConfig("Amount of damage to absorb per stack (hyperbolic).", AutoConfigFlags.PreventNetMismatch, 0f, 1f)]
+        [AutoConfig("Amount of damage to convert per stack (hyperbolic).", AutoConfigFlags.PreventNetMismatch, 0f, 1f)]
         public float bufferFrac { get; private set; } = 0.1f;
 
         [AutoConfigRoOSlider("{0:N1}", 0f, 60f)]
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
-        [AutoConfig("Time over which each damage instance is delayed, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
+        [AutoConfig("Time over which each barrier instance is delayed, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
         public float bufferDuration { get; private set; } = 3f;
 
         [AutoConfigRoOSlider("{0:N1}", 0f, 10f)]
         [AutoConfigUpdateActions(AutoConfigUpdateActionTypes.InvalidateLanguage)]
-        [AutoConfig("Tick interval of the damage buffer, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
+        [AutoConfig("Tick interval of the granted barrier, in seconds.", AutoConfigFlags.PreventNetMismatch, 0f, float.MaxValue)]
         public float bufferRate { get; private set; } = 0f;
-
-        [AutoConfigRoOCheckbox()]
-        [AutoConfig("If active, buffered damage cannot reduce a character below 1 health.", AutoConfigFlags.PreventNetMismatch)]
-        public bool nonlethal { get; private set; } = true;
-
-        [AutoConfigRoOSlider("{0:P0}", 0f, 1f)]
-        [AutoConfig("Cleanse effects (e.g. Blast Shower) will clear this fraction of buffered damage.", AutoConfigFlags.PreventNetMismatch, 0f, 1f)]
-        public float cleanseable { get; private set; } = 1f;
 
 
 
@@ -185,107 +177,36 @@ namespace ThinkInvisible.TinkersSatchel {
 
         public override void Install() {
             base.Install();
-            IL.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
-            IL.RoR2.HealthComponent.Heal += HealthComponent_Heal;
-            On.RoR2.Util.CleanseBody += Util_CleanseBody;
+            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage; ;
         }
 
         public override void Uninstall() {
             base.Uninstall();
-            IL.RoR2.HealthComponent.TakeDamage -= HealthComponent_TakeDamage;
-            IL.RoR2.HealthComponent.Heal -= HealthComponent_Heal;
-            On.RoR2.Util.CleanseBody -= Util_CleanseBody;
+            On.RoR2.HealthComponent.TakeDamage -= HealthComponent_TakeDamage;
         }
 
 
 
         ////// Hooks //////
 
-        private void HealthComponent_TakeDamage(ILContext il) {
-            ILCursor c = new(il);
-
-            int locIndex = 0;
-            if(c.TryGotoNext(
-                x => x.MatchLdloc(out locIndex),
-                x => x.MatchLdcR4(0),
-                x => x.MatchBleUn(out _),
-                x => x.MatchLdarg(0),
-                x => x.MatchLdfld<HealthComponent>(nameof(HealthComponent.barrier))
-                )
-                && c.TryGotoPrev(MoveType.After,
-                x => x.MatchStloc((byte)locIndex))
-                ) {
-                c.Emit(OpCodes.Ldarg_0);
-                c.Emit(OpCodes.Ldarg_1);
-                c.Emit(OpCodes.Ldloc_S, (byte)locIndex);
-                c.EmitDelegate<Func<HealthComponent, DamageInfo, float, float>>((hc, damageInfo, origFinalDamage) => {
-                    if(!hc || damageInfo == null || (damageInfo.damageType & DamageType.FallDamage) != 0) return origFinalDamage;
-                    var count = GetCount(hc.body);
-                    if(count <= 0) return origFinalDamage;
-                    var cpt = hc.GetComponent<DelayedDamageBufferComponent>();
-                    if(!cpt) cpt = hc.gameObject.AddComponent<DelayedDamageBufferComponent>();
-                    if(cpt.isApplying) return origFinalDamage;
-                    var frac = Mathf.Clamp01(1f-1f/(1f + bufferFrac * (float)count));
-                    var reduc = origFinalDamage * frac;
-                    cpt.ApplyDamage(reduc);
-                    return origFinalDamage - reduc;
-                });
-                c.Emit(OpCodes.Stloc_S, (byte)locIndex);
-            } else {
-                TinkersSatchelPlugin._logger.LogError("Failed to apply IL patch (target instructions not found): DamageBuffer::HealthComponent_TakeDamage");
-            }
-        }
-
-        private void HealthComponent_Heal(ILContext il) {
-            ILCursor c = new(il);
-
-            int locIndex = 0;
-            if(c.TryGotoNext(MoveType.Before,
-                x => x.MatchLdloc(out locIndex),
-                x => x.MatchLdcR4(0),
-                x => x.MatchBleUn(out _),
-                x => x.MatchLdarg(out _),
-                x => x.MatchBrfalse(out _),
-                x => x.MatchLdarg(out _),
-                x => x.MatchLdflda<HealthComponent>(nameof(HealthComponent.itemCounts)),
-                x => x.MatchLdfld<HealthComponent.ItemCounts>(nameof(HealthComponent.ItemCounts.barrierOnOverHeal))
-                )) {
-                c.Emit(OpCodes.Ldarg_0);
-                c.Emit(OpCodes.Ldloc, locIndex);
-                c.EmitDelegate<Action<HealthComponent, float>>((hc, overheal) => {
-                    if(!hc) return;
-                    var cpt = hc.GetComponent<DelayedDamageBufferComponent>();
-                    if(cpt)
-                        cpt.ApplyOverheal(overheal);
-                });
-            } else {
-                TinkersSatchelPlugin._logger.LogError("Failed to apply IL patch (target instructions not found): DamageBuffer::HealthComponent_Heal");
-            }
-        }
-
-        private void Util_CleanseBody(On.RoR2.Util.orig_CleanseBody orig, CharacterBody characterBody, bool removeDebuffs, bool removeBuffs, bool removeCooldownBuffs, bool removeDots, bool removeStun, bool removeNearbyProjectiles) {
-            orig(characterBody, removeDebuffs, removeBuffs, removeCooldownBuffs, removeDots, removeStun, removeNearbyProjectiles);
-            if(cleanseable > 0f && removeDots && characterBody && characterBody.healthComponent
-                && characterBody.healthComponent.TryGetComponent<DelayedDamageBufferComponent>(out var ddbc)) {
-
-                ddbc.bufferDamage.Clear();
-                for(var i = 0; i < ddbc.bufferDamage.Count; i++) {
-                    ddbc.bufferDamage[i] = (
-                        ddbc.bufferDamage[i].curr - ddbc.bufferDamage[i].max * cleanseable,
-                        ddbc.bufferDamage[i].max
-                        );
-                }
-                ddbc.bufferDamage.RemoveAll(x => x.curr <= 0f);
-            }
+        private void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo) {
+            orig(self, damageInfo);
+            if(!self || !self.body || damageInfo == null || (damageInfo.damageType & DamageType.FallDamage) != 0) return;
+            var count = GetCount(self.body);
+            if(count <= 0) return;
+            var cpt = self.GetComponent<DelayedBarrierComponent>();
+            if(!cpt) cpt = self.gameObject.AddComponent<DelayedBarrierComponent>();
+            var frac = Mathf.Clamp01(1f - 1f / (1f + bufferFrac * (float)count));
+            var reduc = damageInfo.damage * frac;
+            cpt.ApplyDamage(reduc);
         }
     }
 
     [RequireComponent(typeof(HealthComponent))]
-    public class DelayedDamageBufferComponent : MonoBehaviour {
+    public class DelayedBarrierComponent : MonoBehaviour {
         HealthComponent hc;
         public List<(float curr, float max)> bufferDamage = new();
         float stopwatch = 0f;
-        public bool isApplying { get; private set; } = false;
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by Unity Engine.")]
         void Awake() {
@@ -306,37 +227,8 @@ namespace ThinkInvisible.TinkersSatchel {
                         bufferDamage[i] = (bufferDamage[i].curr - rem, bufferDamage[i].max);
                     }
                     bufferDamage.RemoveAll(x => x.curr <= 0f);
-                    isApplying = true;
 
-                    if(accum > 0f && hc.barrier > 0f) {
-                        if(accum <= hc.barrier) {
-                            hc.Networkbarrier = hc.barrier - accum;
-                            accum = 0f;
-                        } else {
-                            accum -= hc.barrier;
-                            hc.Networkbarrier = 0f;
-                        }
-                    }
-                    if(accum > 0f && hc.shield > 0f) {
-                        if(accum <= hc.shield) {
-                            hc.Networkshield = hc.shield - accum;
-                            accum = 0f;
-                        } else {
-                            accum -= hc.shield;
-                            hc.Networkshield = 0f;
-                            EffectManager.SpawnEffect(HealthComponent.AssetReferences.shieldBreakEffectPrefab, new EffectData {
-                                origin = base.transform.position,
-                                scale = hc.body ? hc.body.radius : 1f
-                            }, true);
-                        }
-                    }
-                    if(accum > 0f) {
-                        var newHealth = hc.Networkhealth - accum;
-                        if(newHealth < 1f && DamageBuffer.instance.nonlethal) newHealth = 1f;
-                        hc.Networkhealth = newHealth;
-                    }
-
-                    isApplying = false;
+                    hc.AddBarrier(accum);
                 }
             }
         }
@@ -344,18 +236,6 @@ namespace ThinkInvisible.TinkersSatchel {
         public void ApplyDamage(float amount) {
             if(amount > 0f)
                 bufferDamage.Add((amount, amount));
-        }
-
-        public void ApplyOverheal(float amount) {
-            if(bufferDamage.Count == 0 || amount <= 0f) return;
-            var total = bufferDamage.Sum(x => x.curr);
-            var frac = amount / total;
-            for(var i = 0; i < bufferDamage.Count; i++) {
-                var reduc = bufferDamage[i].curr * frac;
-                var remaining = bufferDamage[i].curr - reduc;
-                bufferDamage[i] = (remaining, Mathf.Max(bufferDamage[i].max - reduc, remaining));
-            }
-            bufferDamage.RemoveAll(x => x.curr <= 0f);
         }
     }
 }
